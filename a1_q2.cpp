36a64551385e9ec3d708917f417b08780288c686
Shreyakaushik2005 <122819914+Shreyakaushik2005@users.noreply.github.com>
2023-01-17 20:35:47 +0530

    Update a1_q2.cpp

--- a1_q2.cpp
+++ a1_q2.cpp
@@ -9,17 +9,18 @@ int main(){
     if(mark<=100 && mark>90){
        cout<<"excellent";
     }
-    if(mark<=90 && mark>80){
+    else if(mark<=90 && mark>80){
        cout<<"good";
     }
-if(mark<=80 && mark>70){
+   else if(mark<=80 && mark>70){
        cout<<"fair";
     }
-if(mark<=70 && mark>60){
+   else if(mark<=70 && mark>60){
        cout<<"meets expectation";
     }
-  if(mark<=60 ){
+    else(mark<=60 ){
        cout<<"below par";
     }
+    return 0;
 
 }
