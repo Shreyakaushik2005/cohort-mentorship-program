95f183276c2669efc19d85062af65ce79881f5e9
Shreyakaushik2005 <122819914+Shreyakaushik2005@users.noreply.github.com>
2023-01-23 13:51:02 +0530

    Create a2_q14.cpp
    Submitting question 14 of assignment 2

---
+++ a2_q14.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i,x;
+    cout<<"enter a no.";
+    cin>>x;
+    
+    for(i=1;i<=10;i++){
+    
+        cout<<x<<"*"<<i<<"="<<i*x<<endl;
+    }
+}
